8162cbcbe2bfab20809d5335a291a56302b8a716
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-11-10 13:28:09 -0800

    Basic Day Card setup

--- src/components/Daily.jsx
+++ src/components/Daily.jsx
@@ -1,18 +1,25 @@
 import { useSelector } from "react-redux";
 import { getPrettyParameterName } from "../utils/parameters";
+import { Card } from "@mui/material";
 
 export default function Daily({ weather, pageId, sectionId, cardId }) {
 
-    const selectedDailyParameters = useSelector(state => state.dashboard.cards[cardId].selectedDailyParameters);    
+    const selectedDailyParameters = useSelector(state => state.dashboard.cards[cardId].selectedDailyParameters);
+    const dailyRange = useSelector(state => state.dashboard.cards[cardId].dailyRange);  
+    const days = Array.from({length: dailyRange[1] - dailyRange[0] + 1}, (_, i) => dailyRange[0] + i - 1); 
 
     return (
-        <div>
-            <h1>Daily</h1>
-            <ul>
-                {selectedDailyParameters.map((param) => {
-                    return <li key={param}>{getPrettyParameterName(param)}: {weather.daily.weatherVariables[param].data}</li>;
-                })}
-            </ul>
+        <div className="flex flex-wrap gap-1">
+            {days.map(day => (
+                <Card key={day}>
+                    <h1>{weather.daily.time[day]}</h1>
+                    {selectedDailyParameters.map(param => (
+                        <p key={param}>
+                        {weather.daily.weatherVariables[param].data[day]}
+                        </p>
+                    ))}
+                </Card>
+            ))}
         </div>
     );
 }
